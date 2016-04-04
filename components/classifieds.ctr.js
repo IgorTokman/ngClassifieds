d1397c01ea8a2b3e882d8f94b0d4d9689bc16307
IgorTokman <igortokman1994@ukr.net>
2016-04-04 13:26:13 +0300

    implemented the full page review

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -6,12 +6,115 @@
         .module("ngClassifieds")
         .controller("classifiedsCtrl", function($scope){
 
-            $scope.classified = {
-                title:  "mp3 player",
-                price:  "$100",
-                url:    "http://nl.pcmweb.s3-eu-west-1.amazonaws.com/thumbnails/980/024d2/18973951_640.jpg",
-                desc:   "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa deserunt impedit magni optio quisquam, quos suscipit ut. A ab amet culpa deleniti eum, explicabo illo iure, maxime omnis, possimus quae."
-            }
-
+            $scope.classifieds = [
+                {
+                "id":       1,
+                "title":    "Lorem ipsum",
+                "desc":     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa deserunt impedit magni optio quisquam, quos suscipit ut. A ab amet culpa deleniti eum, explicabo illo iure, maxime omnis, possimus quae.",
+                "price":    200,
+                "posted":   "2015-10-15",
+                "contact":  {
+                    "name":     "John Smith",
+                    "phone":    "+1234567890",
+                    "email":    "John@Smith.com"
+                },
+                "categories":[
+                    "Vehicles",
+                    "Parts and Accessories"
+                ],
+                "image":    "http://nl.pcmweb.s3-eu-west-1.amazonaws.com/thumbnails/980/024d2/18973951_640.jpg",
+                "views":    123,
+            },
+                {
+                "id":       2,
+                "title":    "Lorem ipsum",
+                "desc":     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa deserunt impedit magni optio quisquam, quos suscipit ut. A ab amet culpa deleniti eum, explicabo illo iure, maxime omnis, possimus quae.",
+                "price":    200,
+                "posted":   "2015-10-15",
+                "contact":  {
+                    "name":     "John Smith",
+                    "phone":    "+1234567890",
+                    "email":    "John@Smith.com"
+                },
+                "categories":[
+                    "Vehicles",
+                    "Parts and Accessories"
+                ],
+                "image":    "http://nl.pcmweb.s3-eu-west-1.amazonaws.com/thumbnails/980/024d2/18973951_640.jpg",
+                "views":    123,
+            },
+                {
+                "id":       3,
+                "title":    "Lorem ipsum",
+                "desc":     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa deserunt impedit magni optio quisquam, quos suscipit ut. A ab amet culpa deleniti eum, explicabo illo iure, maxime omnis, possimus quae.",
+                "price":    200,
+                "posted":   "2015-10-15",
+                "contact":  {
+                    "name":     "John Smith",
+                    "phone":    "+1234567890",
+                    "email":    "John@Smith.com"
+                },
+                "categories":[
+                    "Vehicles",
+                    "Parts and Accessories"
+                ],
+                "image":    "http://nl.pcmweb.s3-eu-west-1.amazonaws.com/thumbnails/980/024d2/18973951_640.jpg",
+                "views":    123,
+            },
+                {
+                "id":       4,
+                "title":    "Lorem ipsum",
+                "desc":     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa deserunt impedit magni optio quisquam, quos suscipit ut. A ab amet culpa deleniti eum, explicabo illo iure, maxime omnis, possimus quae.",
+                "price":    200,
+                "posted":   "2015-10-15",
+                "contact":  {
+                    "name":     "John Smith",
+                    "phone":    "+1234567890",
+                    "email":    "John@Smith.com"
+                },
+                "categories":[
+                    "Vehicles",
+                    "Parts and Accessories"
+                ],
+                "image":    "http://nl.pcmweb.s3-eu-west-1.amazonaws.com/thumbnails/980/024d2/18973951_640.jpg",
+                "views":    123,
+            },
+                {
+                "id":       5,
+                "title":    "Lorem ipsum",
+                "desc":     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa deserunt impedit magni optio quisquam, quos suscipit ut. A ab amet culpa deleniti eum, explicabo illo iure, maxime omnis, possimus quae.",
+                "price":    200,
+                "posted":   "2015-10-15",
+                "contact":  {
+                    "name":     "John Smith",
+                    "phone":    "+1234567890",
+                    "email":    "John@Smith.com"
+                },
+                "categories":[
+                    "Vehicles",
+                    "Parts and Accessories"
+                ],
+                "image":    "http://nl.pcmweb.s3-eu-west-1.amazonaws.com/thumbnails/980/024d2/18973951_640.jpg",
+                "views":    123,
+            },
+                {
+                "id":       6,
+                "title":    "Lorem ipsum",
+                "desc":     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa deserunt impedit magni optio quisquam, quos suscipit ut. A ab amet culpa deleniti eum, explicabo illo iure, maxime omnis, possimus quae.",
+                "price":    200,
+                "posted":   "2015-10-15",
+                "contact":  {
+                    "name":     "John Smith",
+                    "phone":    "+1234567890",
+                    "email":    "John@Smith.com"
+                },
+                "categories":[
+                    "Vehicles",
+                    "Parts and Accessories"
+                ],
+                "image":    "http://nl.pcmweb.s3-eu-west-1.amazonaws.com/thumbnails/980/024d2/18973951_640.jpg",
+                "views":    123,
+            },
+            ]
         });
 })();
